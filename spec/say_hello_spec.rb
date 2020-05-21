3fc6757ad3c7c5fcc47cd79542b65c3e711db9f9
Joshua Ortega <joshua.ortega13@gmail.com>
2020-05-21 20:56:41 +0000

    Automatically backed up by Learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -5,7 +5,7 @@ describe "say_hello" do
   it 'accepts an argument of a name and prints out Hello with that Name' do
     expect($stdout).to receive(:puts).with("Kent Beck")
     say_hello()
-    
+
   end
 
   it 'defaults to Ruby Programmer when no name is passed in' do
